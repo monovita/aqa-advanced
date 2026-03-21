50b83c349e5a08fea8107b3c585b40455bbc7c5b
monovita <depointon@gmail.com>
2026-03-21 19:53:07 +0200

    On branch branch-homework-5-1 created folder folderHW5 with 3 files

---
+++ folderHW5/task3.js
@@ -0,0 +1,22 @@
+console.log(`
+    Multiplication table by for loop
+    `);
+
+let numberFor = 4;
+
+for (i = 1; i <= 10; i++){
+    let result = numberFor * i;
+    console.log(`${numberFor} x ${i} = ${result}`);
+}
+
+console.log(`
+    Multiplication table by while loop
+    `);
+
+let numberWhile = 2;
+i = 1;
+while (i <= 10) {
+    let result = numberWhile * i;
+    console.log(`${numberWhile} x ${i} = ${result}`);
+    i++;
+}
