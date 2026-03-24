fd0c368f9dfd76adf6bb5503e6c2a112cb183a36
monovita <depointon@gmail.com>
2026-03-24 20:33:15 +0200

    Created folder folderHW6 with 3 files

---
+++ folderHW6/task3.js
@@ -0,0 +1,17 @@
+function checkOrder (available, ordered) {
+    switch(true) {
+        case available < ordered && ordered > 0:
+            console.log("Your order is too large, we don’t have enough goods.");
+            break;
+        case ordered <= 0:
+            console.log("Your order is empty");
+            break;
+        case available >= ordered && ordered > 0:
+            console.log("Your order is accepted");
+            break;
+        default:
+            console.log("Please enter valid numbers");
+    }
+}
+
+checkOrder(10, 5);
