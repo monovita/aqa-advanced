6217de5038da55cca2a4deffd43b602989ea19b4
monovita <depointon@gmail.com>
2026-03-28 16:29:48 +0200

    Created folder folderHW7 with 3 files

---
+++ folderHW7/task1.js
@@ -0,0 +1,13 @@
+const handleNum = (number, callbackEven, callbackOdd) => {
+    const isEven = number % 2 === 0 ? callbackEven : callbackOdd;
+    return isEven();
+}
+
+const handleEven = () => { console.log('number is even'); }
+const handleOdd = () => { console.log('number is odd'); }
+
+handleNum(2, handleEven, handleOdd);
+
+console.log('------------------------');
+
+handleNum(3, handleEven, handleOdd);
---
+++ folderHW7/task2.js
@@ -0,0 +1,9 @@
+const outputNumber = (num) => {
+    if (num <= 0) return;
+    console.log(num);
+    outputNumber(num - 1);
+}
+
+outputNumber (5);
+
+
