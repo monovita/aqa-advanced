915d215babbd4df21beaf49f05d1295be5ce88b1
monovita <depointon@gmail.com>
2026-04-25 20:30:29 +0300

    Installed jest, axious, and added test files to main folder

--- TestFolderOnMain/user.test.js
+++ TestFolderOnMain/user.test.js
@@ -1,16 +1,18 @@
 const axios = require('axios');
 
-test("User can login successfully", async () => {
-    var response = await axios.post(`http://dmoney.roadtocareer.net/user/login/user/login`,
-        {
-            "email": "salman@roadtocareer.net",
-            "password": "1234"
-        },
-        {
-            headers: {
-                "Content-Type": "application/json",
-            }
-        })
-    console.log(response.data)
-    expect(response.data.message).toContain("Login successfully")
-})
+test('User can login successfully', async () => {
+	var response = await axios.post(
+		`http://dmoney.roadtocareer.net/user/login/user/login`,
+		{
+			email: 'salman@roadtocareer.net',
+			password: '1234',
+		},
+		{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		},
+	);
+	console.log(response.data);
+	expect(response.data.message).toContain('Login successfully');
+});
