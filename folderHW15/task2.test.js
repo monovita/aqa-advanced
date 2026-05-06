2907618e36841bc5d3538d35a3631f5adc445a19
monovita <depointon@gmail.com>
2026-05-06 15:30:36 +0300

    Created folder folderHW15 with 3 files

--- folderHW15/task2.test.js
+++ folderHW15/task2.test.js
@@ -1,34 +1,31 @@
 const axios = require('axios');
 
-const customHeaderValue = { 'Content-type': "ABC123_HEADER" };
-const customParamsValue = { param1: "ABC123_PARAM" };
+const customHeaderValue = { 'Content-type': 'ABC123_HEADER' };
+const customParamsValue = { param1: 'ABC123_PARAM' };
 const mockedApi = axios.create();
 
-function headersURL(){
-return mockedApi.get(
-        'https://jsonplaceholder.typicode.com/posts/1',
-        {
-            headers: customHeaderValue,
-            params: customParamsValue,
-        },
-    );
+function headersURL() {
+	return mockedApi.get('https://jsonplaceholder.typicode.com/posts/1', {
+		headers: customHeaderValue,
+		params: customParamsValue,
+	});
 }
 
 test('Test Request Headers and Params', async () => {
-  await new Promise((resolve, reject) => {
-    mockedApi.interceptors.request.use((config) => {
-      try {
-        console.log(config.headers);
-        expect(config.headers.get('Content-type')).toBe('ABC123_HEADER'); 
-        console.log(config.params);
-        expect(config.params).toEqual(customParamsValue);                  
-        resolve();
-      } catch (error) {
-        reject(error); 
-      }
-      return config;
-    });
+	await new Promise((resolve, reject) => {
+		mockedApi.interceptors.request.use((config) => {
+			try {
+				console.log(config.headers);
+				expect(config.headers.get('Content-type')).toBe('ABC123_HEADER');
+				console.log(config.params);
+				expect(config.params).toEqual(customParamsValue);
+				resolve();
+			} catch (error) {
+				reject(error);
+			}
+			return config;
+		});
 
-    headersURL();
-  });
+		headersURL();
+	});
 });
--- folderHW15/task3.test.js
+++ folderHW15/task3.test.js
@@ -3,22 +3,21 @@ const axios = require('axios');
 jest.mock('axios');
 
 function mockedURL() {
-  return axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.data);
+	return axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => response.data);
 }
 
 test('Mocked successful GET request', async () => {
-  const mockData = [{ id: 1, title: 'sunt aut facere' }];
+	const mockData = [{ id: 1, title: 'sunt aut facere' }];
 
-  axios.get.mockResolvedValue({ data: mockData });
+	axios.get.mockResolvedValue({ data: mockData });
 
-  const response = await mockedURL();
+	const response = await mockedURL();
 
-  expect(response[0].title).toContain('sunt aut facere');
+	expect(response[0].title).toContain('sunt aut facere');
 });
 
 test('Mocked failed GET request', async () => {
-  axios.get.mockRejectedValue(new Error('Network Error')); 
+	axios.get.mockRejectedValue(new Error('Network Error'));
 
-  await expect(mockedURL()).rejects.toThrow('Network Error'); 
+	await expect(mockedURL()).rejects.toThrow('Network Error');
 });
